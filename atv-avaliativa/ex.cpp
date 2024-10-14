df9256573c3ebccaf53e7569c7ffd8596e753814
M4tiasToreta <toretamatias218@gmail.com>
2024-10-14 18:15:31 -0300

    first part atv

--- atv-avaliativa/ex.cpp
+++ atv-avaliativa/ex.cpp
@@ -6,13 +6,13 @@ int main() {
     for (int i=0; i<3; i++){
         printf("Digite o nome%d:\n", i+1);
         if (i==0) {
-            scanf("Digite o nome 1: %s", nomes[i]);
+            scanf("%s", nomes[i]);
         }
         if (i==1) {
-            scanf("Digite o nome 2: %s", nomes[i]);
+            scanf("%s", nomes[i]);
         }
         if (i==2) {
-            scanf("Digite o nome 3: %s", nomes[i]);
+            scanf("%s", nomes[i]);
         }
 
         printf("%s", nomes[2]);
